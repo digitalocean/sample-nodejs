ef1724f0f615f8057aa5eeb2d1223890be7ab8ee
sack31337 <58873368+sack31337@users.noreply.github.com>
2025-01-07 17:47:21 -0800

    Update app.js
    .

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
-// view engine setup
+// view engine setup.
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
